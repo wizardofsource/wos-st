a6caddde56d1a161ac99766d00eca0962fce956b
wos <wizardofsource@protonmail.com>
2020-06-10 08:05:46 +0200

    Changed font, updated README.md

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Visit [www.suckless.org](https://st.suckless.org/) for more info on st.
 
 Applied patches:
 * [Gruvbox theme](https://st.suckless.org/patches/gruvbox/)
+* [Ligatures](https://st.suckless.org/patches/ligatures/)
 
 Personal tastes: 
 * Uses font [Fira Code Medium Nerd Font Complete Mono](https://www.nerdfonts.com/font-downloads)
--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 //static char *font = "Liberation Mono:pixelsize=16:antialias=true:autohint=true";
-static char *font = "FuraCode Nerd Font Mono:style=Medium,Regular:size=16";
+static char *font = "FiraCode Nerd Font Mono:style=Medium,Regular:size=16";
 static int borderpx = 4;
 
 /*
